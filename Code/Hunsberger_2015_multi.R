d28e77b41c1a46e34080c26b9ffc74bff370773a
Trang Tu <trang.tp.hien@gmail.com>
2026-01-30 14:58:58 +0800

    Revise bootstrap_ER and scan_p_value
    scan_p_value: the null distribution takes into account reflect censoring
    bootstrap_ER: only execute when n_event >5
    Add function to make result table from result lists

--- Code/Hunsberger_2015_multi.R
+++ Code/Hunsberger_2015_multi.R
@@ -22,19 +22,11 @@
 
 
 # Load necessary packages
-if (!require("pacman", quietly = TRUE)) {
-  install.packages("pacman")
-}
+library(here)
 
-library(pacman)
-pacman::p_load(
-rio, 
-here
-)
-
-# ------------------------------------------------------------------------------
+################################################################################
 # Part 1: Define functions to implement the method Hunsberger (2015) -----------
-# ------------------------------------------------------------------------------
+################################################################################
 
 ## 1. Function for data preparation --------------------------------------------
 # This function loops over 4 exposure columns
@@ -46,37 +38,65 @@ prepare_scan_data_multi <- function(data, id_col, event_col, exposure_cols,
     bnt_1  = "bnt_2",
     chad_1 = "chad_2")
   
-  
   # Iterate over each exposure column provided
   results <- lapply(exposure_cols, function(exp_col) {
     
     temp_data <- data
+    
+    # Restrict to individuals who received this exposure within the observation period
+    exposed <- !is.na(temp_data[[exp_col]]) &
+      temp_data[[exp_col]] >= temp_data[[start_col]] &
+      temp_data[[exp_col]] <= temp_data[[end_col]]
+    
+    temp_data <- temp_data[exposed, ]
+    
+    # Stop if filtered data are empty
+    if (nrow(temp_data) == 0) {
+      return(list(
+        event_days   = numeric(0),
+        daily_counts = integer(max_days),
+        risk_set     = integer(max_days),
+        n            = 0,
+        m            = max_days
+      ))
+    }
+    
     # Calculate event time: day 0 = day of specific exposure
     temp_data$event_time <- temp_data[[event_col]] - temp_data[[exp_col]]
     
-    # Default: user-specified max_days for everyone
-    temp_data$max_days_eff <- max_days
+    # Censor max_days at end_col, or for dose 1: min(end_col, dose-2)
     
-    # Apply dose-2 censoring if this is a first dose
+    censor_date <- temp_data[[end_col]]
+    
     if (exp_col %in% names(dose_pairs)) {
-      
       dose2_col <- dose_pairs[[exp_col]]
-      has_dose2 <- !is.na(temp_data[[dose2_col]]) & 
-        !is.na(temp_data[[exp_col]])
-      # Calculate dosing interval
-      dose_gap <- temp_data[[dose2_col]] - temp_data[[exp_col]] - 1
-      # Censor max_days at dose 2
-      temp_data$max_days_eff[has_dose2] <- 
-        pmin(max_days, dose_gap[has_dose2])
+      has_dose2 <- !is.na(temp_data[[dose2_col]])
+      
+      censor_date[has_dose2] <-
+        pmin(censor_date[has_dose2], temp_data[[dose2_col]][has_dose2] - 1)
     }
     
-    # Filter conditions
+    # Individual-specific max follow-up (days since exposure)
+    temp_data$max_days_eff <- pmin(
+      max_days,
+      censor_date - temp_data[[exp_col]]
+    )
+    
+    # In case of non-positive max_days_eff, set it to 0
+    temp_data$max_days_eff[temp_data$max_days_eff < 0] <- 0
+    
+    # Construct the risk set: number exposed individuals who are not censored at day t
+    risk_set <- tabulate(
+      unlist(lapply(temp_data$max_days_eff, seq_len)),
+      nbins = max_days
+    ) 
+    
+    # Filter events: 
     # 1. Event must be between the start and end of observation period
     # 2. event_time must be within [1, max_days]
-    # 3. no missing values in critical columns (event, exposure)
+    # 3. no missing values in event, exposure
          
-    valid_rows <- !is.na(temp_data$event_time) & 
-      !is.na(temp_data[[event_col]]) &
+    valid_rows <- !is.na(temp_data[[event_col]]) &
       temp_data[[event_col]] >= temp_data[[start_col]] & 
       temp_data[[event_col]] <= temp_data[[end_col]] &
       temp_data$event_time >= 1 & 
@@ -84,11 +104,9 @@ prepare_scan_data_multi <- function(data, id_col, event_col, exposure_cols,
          
     df <- temp_data[valid_rows, ]
     
-    # Sort by ID and event time
-    df <- df[order(df[[id_col]], df$event_time), ]
-    
-    # Keep only the first event for each case ID for this exposure 
-    df <- df[!duplicated(df[[id_col]]), ]
+    # Keep only the first event for each case ID for this exposure (we do not need this because we already select first event in data cleaning)
+    #df <- df[order(df[[id_col]], df$event_time), ]
+    #df <- df[!duplicated(df[[id_col]]), ]
     
     # Tabulate counts
     counts <- tabulate(df$event_time, nbins = max_days)
@@ -96,8 +114,9 @@ prepare_scan_data_multi <- function(data, id_col, event_col, exposure_cols,
     return(list(
       event_days = df$event_time,
       daily_counts = counts,
+      risk_set = risk_set,
       n = sum(counts),
-      m = max_days
+      m = max_days 
     ))
   })
   
@@ -165,19 +184,22 @@ find_max_glrt_scan <- function(counts # Daily event counts up to day m
 ## 4. Function to calculate the Monte Carlo p-value ----------------------------
 # for testing whether events occur earlier than would be expected 
 # if events follow a uniform distribution
-scan_p_value <- function(counts, n_sim = 4999, seed = 2026) {
+scan_p_value <- function(counts, risk_set, n_sim = 4999, seed = 2026) {
   
   obs <- find_max_glrt_scan(counts) # The observed test stat
   m <- length(counts)
   n <- sum(counts)
   
-  set.seed(seed)
+  # Calculate the probability of event being allocated to day t:
+  # Under the null, event hazard is constant over observed person-time, conditional on exposure
+  # In other words, probability of event occurring on day t is proportional to how much person-time exists on day t.
+  prob <- risk_set / sum(risk_set)
+  
   # Simulate the test stat distribution under the null
+  set.seed(seed)
   sim_stats <- replicate(n_sim, {
-    # Simulate event time for n individual under the null (uniform distribution)
-    sim_event_time <- sample(1:m, size = n, replace = TRUE) 
-    # Calculate the event count per day
-    sim_counts <- tabulate(sim_event_time, nbins = m)
+    # simulate counts per day using a multinomial distribution with weights equal to daily risk set sizes
+    sim_counts <- as.vector(rmultinom(1, size = n, prob = prob))   
     # Calculate the test stat under the null
     find_max_glrt_scan(sim_counts)$max_log_glr_stat
   })
@@ -196,11 +218,18 @@ calculate_ER <- function(Ec, c, n, m) {
 }
 
 ## 6. Non-parametric bootstrap for 95% CI of ER --------------------------------
-bootstrap_ER <- function(event_time, m, n_boot = 5000, seed = 2026) {
+bootstrap_ER <- function(event_time, m, n_boot = 5000, seed = 2026, min_events = 5) {
   
   n <- length(event_time)
+  
+  # Only calculate 95% CI if there are at least 5 events in the risk window 
+  if (n < min_events) {
+    return(c(lower = NA_real_, upper = NA_real_))
+  }
+  
   ERs <- numeric(n_boot) # Vector to store the bootstraped ERs
   
+  set.seed(seed)
   # Bootstrap the ER 
   for (b in 1:n_boot) {
     # Resample the observed event time for n individuals
@@ -228,18 +257,20 @@ run_scan_test <- function(prepared_data,
                           seed = 2026) {
   # Prepare data
   counts <- prepared_data$daily_counts
+  risk_set <- prepared_data$risk_set
   n <- prepared_data$n
   m <- prepared_data$m
   event_days <- prepared_data$event_days
   
   # Scan to choose the optimal window and calculate the p-value
-  scan_res <- scan_p_value(counts, n_sim, seed)
+  scan_res <- scan_p_value(counts = counts, risk_set = risk_set, 
+                           n_sim = n_sim, seed = seed)
   
   # Calculate ER
   ER_hat <- calculate_ER(
-    scan_res$observed$optimal_Ec,
-    scan_res$observed$optimal_c,
-    n, m
+    Ec = scan_res$observed$optimal_Ec,
+    c = scan_res$observed$optimal_c,
+    n = n, m = m
   )
   
   # Calculate the 95% CI of ER
@@ -275,7 +306,7 @@ run_full_analysis <- function(data, id_col, event_col, exposure_cols, start_col,
     }
     
     # Execute run_scan_test logic
-    res <- run_scan_test(prep, n_sim = n_sim, n_boot = n_boot, seed = seed)
+    res <- run_scan_test(prepared_data = prep, n_sim = n_sim, n_boot = n_boot, seed = seed)
     res$exposure <- name
     return(res)
   })
@@ -284,9 +315,53 @@ run_full_analysis <- function(data, id_col, event_col, exposure_cols, start_col,
   return(final_results)
 }
 
-# ------------------------------------------------------------------------------
+## 9. Functions to make result table -------------------------------------------
+
+# Only extract the field if it exists
+get_or_na <- function(x, name) {
+  if (!is.null(x[[name]])) x[[name]] else NA
+}
+
+results_to_table <- function(result_list) {
+  
+  rows <- list()
+  
+  for (outcome in names(result_list)) {
+    for (exposure in names(result_list[[outcome]])) {
+      
+      res <- result_list[[outcome]][[exposure]]
+      
+      er_ci_low <- NA
+      er_ci_up  <- NA
+      
+      if (!is.null(res$er_ci_95)) {
+        er_ci_low <- unname(res$er_ci_95["lower"])
+        er_ci_up  <- unname(res$er_ci_95["upper"])
+      }
+      #append the data frame as a new element at the end of the list
+      rows[[length(rows) + 1]] <- data.frame(
+        outcome              = outcome,
+        exposure             = exposure,
+        error                = get_or_na(res, "error"),
+        total_event          = get_or_na(res, "total_event"),
+        optimal_window_days  = get_or_na(res, "optimal_window_days"),
+        p_value              = get_or_na(res, "p_value"),
+        events_in_window     = get_or_na(res, "events_in_window"),
+        excess_risk          = get_or_na(res, "excess_risk"),
+        er_ci_low            = er_ci_low,
+        er_ci_up             = er_ci_up,
+        stringsAsFactors     = FALSE
+      )
+    }
+  }
+  # combining the list of dataframes
+  do.call(rbind, rows)
+}
+
+
+################################################################################
 # Part 2: Implement the method on the data -------------------------------------
-# ------------------------------------------------------------------------------
+################################################################################
 
 ## Load data -----------------------------------------------------------------
 
@@ -294,7 +369,7 @@ files <- c("individual", "myo_sccs", "pe_sccs", "thrc_sccs", "covid_infection", 
 
 cdm_sccs <- setNames(
   lapply(files, function(nm) {
-    rio::import(here("Processed_data", paste0(nm, ".rds")), trust = TRUE)
+    readRDS(here("Processed_data", paste0(nm, ".rds")))
   }),
   files
 )
@@ -305,35 +380,19 @@ exposures <- c('bnt_1', 'bnt_2', 'chad_1', 'chad_2')
 scan_length <- 70
 
 ## Implement the analysis ------------------------------------------------------
-results <- run_full_analysis(data, "id", "myo", exposures, "obs_sta", "obs_end", scan_length)
+results_huns <- list(myo = NULL, pe = NULL, thrc = NULL)
+
+results_huns[c("myo", "pe", "thrc")] <- lapply(cdm_sccs[c("myo_sccs", "pe_sccs", "thrc_sccs")], function(df) { 
+  run_full_analysis(data = df, 
+                    id_col = "id", event_col = "diagnosis", 
+                    exposure_cols = exposures, 
+                    start_col = "obs_sta", end_col = "obs_end", max_days = scan_length)
+  })
+
+results_huns_tab <- results_to_table(results_huns)
+
+## Export the results
+
+write.csv(results_huns_tab, here("Report", "Results_Hunsberger.csv"))
 
 
-
-## Check data (optional)
-data_prepared <- prepare_scan_data_multi(data = cdm_eligible$pe_sccs, 
-                                         id_col = "id", 
-                                         event_col =  "diagnosis", 
-                                         exposure_cols = exposures, 
-                                         start_col = "obs_sta", end_col = "obs_end", 
-                                         max_days = 185)
-
-# Convert to a summary table
-summary_df <- do.call(rbind, lapply(names(data_prepared), function(name) {
-  x <- data_prepared[[name]]
-  data.frame(
-    exposure = name,
-    total_events = x$n,
-    max_days = x$m
-  )
-}))
-
-print(summary_df)
-
-daily_df <- do.call(rbind, lapply(names(data_prepared), function(name) {
-  x <- data_prepared[[name]]
-  data.frame(
-    exposure = name,
-    day = 1:x$m,
-    count = x$daily_counts
-  )
-}))
