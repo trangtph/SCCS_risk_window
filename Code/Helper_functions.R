744454849f5a1942c65b0ccc462f9f3f5745dfed
Trang Tu <trang.tp.hien@gmail.com>
2026-02-09 15:54:40 +0800

    Add helper functions to detect errors

---
+++ Code/Helper_functions.R
@@ -0,0 +1,23 @@
+###########################################
+### Project: SCCS Risk Window Scanning ####
+### Author: Trang - Azida              ####
+###########################################
+
+###################################
+### Helper functions            ###
+###################################
+
+log_error <- function(msg, 
+                      output_dir = here("Results")) {
+  
+  if (!dir.exists(output_dir)) dir.create(output_dir, recursive = TRUE)
+  
+  log_file <- file.path(output_dir, "error_log.txt")
+  
+  
+  cat(
+    sprintf("[%s] %s\n", Sys.time(), msg),
+    file = log_file,
+    append = TRUE
+  )
+}
